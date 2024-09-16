299a2a5d7f416bdf2b8720a6c7a137330c0281e7
Sher Zahed <sherzahed@gmail.com>
2024-09-16 14:15:05 -0700

    Local training based on ram, weight, and primary storage. Added templates.

--- app.py
+++ app.py
@@ -7,24 +7,27 @@ from flask import Flask, render_template, request
 import joblib
 
 # Data Process
-rentalDF = pd.read_csv("data/rental_1000.csv")
+laptopDF = pd.read_csv("data/laptop_prices.csv")
 
 # Data Transformation
-X = rentalDF[['rooms','sqft']].values #Features
-y = rentalDF['price'].values #Label
+# X = laptopDF[['Company','Product','TypeName','Inches','Ram','OS','Weight','Screen','ScreenW','ScreenH','Touchscreen','IPSpanel','RetinaDisplay','CPU_company','CPU_freq','CPU_model','PrimaryStorage','SecondaryStorage','PrimaryStorageType','SecondaryStorageType','GPU_company','GPU_model']].values #Features
+
+# x_features = laptopDF[['Ram','Weight','GPU_model']].values # Features for RAM, weight, and GPU model
+x_features = laptopDF[['Ram','Weight','PrimaryStorage']].values # Features for RAM, weight, and GPU model
+y = laptopDF['Price_euros'].values # Label
 
 # Split Data into Training and Testing
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+X_train, X_test, y_train, y_test = train_test_split(x_features, y, test_size=0.20)
 
 # Model Training
 model = LinearRegression().fit(X_train,y_train)
 
 
 # Model Save 
-joblib.dump(model,'rental_price_model.joblib')
+joblib.dump(model,'laptop_price_model.joblib')
 
 # Model Load
-model = joblib.load('rental_price_model.joblib')
+model = joblib.load('laptop_price_model.joblib')
 
 # Init Flask application
 app = Flask(__name__)
@@ -35,11 +38,13 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    rooms = int(request.form['rooms'])
-    sqft = int(request.form['sqft'])
+    ram = int(request.form['ram'])
+    weight = float(request.form['weight'])
+    # gpu_model = string(request.form['GPU_model'])
+    primaryStorage = int(request.form['PrimaryStorage'])
 
     # Make prediction
-    prediction = model.predict(np.array([[rooms,sqft]]))
+    prediction = model.predict(np.array([[ram,weight,primaryStorage]]))
 
     return render_template('result.html', prediction=prediction[0])
 
@@ -56,13 +61,13 @@ if __name__ == "__main__":
 # print(f"RMSE: {rmse}") 
 
 # sample_index =0 # Change this index to test different samples 
-# predict_rental_price = model.predict([X_test[sample_index]])[0]
+# predict_laptop_price = model.predict([X_test[sample_index]])[0]
 
-# print(f"The Real Rental Price for Rooms count={X_test[sample_index][0]} and Area in Sqft={X_test[sample_index][1]} is={y_test[sample_index]}")
-# print(f"The Predicted Rental Price for Rooms count={X_test[sample_index][0]} and Area in Sqft={X_test[sample_index][1]} is={predict_rental_price}")
+# print(f"The Real laptop Price for ram count={X_test[sample_index][0]} and Area in weight={X_test[sample_index][1]} is={y_test[sample_index]}")
+# print(f"The Predicted laptop Price for ram count={X_test[sample_index][0]} and Area in weight={X_test[sample_index][1]} is={predict_laptop_price}")
 
-# print("The Actual Rental Price for Rooms with Count =",X_test[0][0]," and","Sqft=",X_test[0][1],"is =", y_test[0])
-# print("The Predicted Rental Price for Rooms with Count =",X_test[0][0]," and","Area in Sqft=",X_test[0][1],"is =",predict_rental_price)
+# print("The Actual laptop Price for ram with Count =",X_test[0][0]," and","weight=",X_test[0][1],"is =", y_test[0])
+# print("The Predicted laptop Price for ram with Count =",X_test[0][0]," and","Area in weight=",X_test[0][1],"is =",predict_laptop_price)
 
 # Metric -RMSE
 # Root Mean Square Error
